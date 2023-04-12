f4c398ba0d06b3e2e95f2ee03213b14086e54291
Retrowwave <114674990+Retrowwave@users.noreply.github.com>
2023-04-12 12:54:44 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+import javax.swing.SpringLayout.Constraints;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+
+        // Declaração de constante que não pode ser alterada
+        final String ola_mundo = "Hello World!";
+
+        System.out.println(ola_mundo);
+        
+    }
+}
